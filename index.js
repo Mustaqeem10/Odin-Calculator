978e2777f7a13b5d80aa8fe75869c00e5d07c5f8
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2022-08-22 12:26:10 +0500

    Operator Function added

--- index.js
+++ index.js
@@ -1,35 +1,50 @@
 // Function to perform addition
-function add (){
+function add() {
     let sum = 0;
-    for (let i = 0 ; i < arguments.length ; i++){
+    for (let i = 0; i < arguments.length; i++) {
         sum = sum + arguments[i];
     }
     return sum;
 }
 // Function to perform subtraction
-function sub (){
+function sub() {
     let ans = arguments[0];
-    for (let i = 1 ; i < arguments.length ; i++){
+    for (let i = 1; i < arguments.length; i++) {
         ans = ans - arguments[i];
     }
     return ans;
     console.log(ans)
 }
 
-function mul (){
+function mul() {
     let product = 1;
-    for (let i = 0 ; i < arguments.length ; i++){
+    for (let i = 0; i < arguments.length; i++) {
         product = product * arguments[i];
     }
     return product;
 }
 
-function div (){
+function div() {
     let rem = arguments[0];
-    for (let i = 1 ; i < arguments.length ; i++){
-        rem = rem/arguments[i];
+    for (let i = 1; i < arguments.length; i++) {
+        rem = rem / arguments[i];
     }
     rem = rem.toFixed(3);
     return rem;
+}
+
+function operate(num1, num2, op) {
+    if (op == '+') {
+        return add(num1, num2);
+    }
+    else if (op == '-') {
+        return sub(num1, num2);
+    }
+    else if (op == '*') {
+        return mul(num1, num2);
+    }
+    else if (op == '/') {
+        return div(num1, num2);
+    }
 }
 
